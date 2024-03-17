671fa6d674cd9fc543c14ebf6f0cc25e81629f1d
VladMrt <handbal783@gmail.com>
2024-03-17 18:17:43 +0200

    bag ceva in cosu asta de cumparaturi ca am stat 12 ore

--- server.py
+++ server.py
@@ -51,34 +51,17 @@ def search_product():
     search_results = search_products_by_keyword(request.form['product'])
     for item in search_results:
         print("Products:", item.get('product_name'))
-    # if search_results:
-    #     products = search_results['products']
-    #     if products:
-    #         product = products[0]
-    #         name = product.get('product_name')
-    #         nutrition = product.get('nutrition_grades_tags')
-    #         ingredients = product.get('ingredients_text')
-    #         image = product.get('image_url')
-    #         print("Product Name:", name)
-    #         print("Ingredients:", nutrition)
-    #         print("Nutrition Facts:", ingredients)
-    #         return render_template('product.html', name=name, ingredients=ingredients, nutrition=nutrition, image=image)
-    #     else:
-    #         print("No products found with the given name.")
-    #         return render_template('test_off_api.html')
+    cart_elements = len(session['cart'])
     if 'username' in session:
         loggedIn = True
-        return render_template('search_product.html', search_results=search_results, username=session['username'], loggedIn=loggedIn)
+        return render_template('search_product.html', search_results=search_results, username=session['username'], loggedIn=loggedIn, cart_elements=cart_elements)
     else:
         loggedIn = False
-        return render_template('search_product.html', search_results=search_results, loggedIn=loggedIn)
-
-    
+        return render_template('search_product.html', search_results=search_results, loggedIn=loggedIn, cart_elements=cart_elements)
     
 
 @app.route('/product_details')
 def product_details():
-    # Assuming you have the HTML content passed from a request or stored somewhere
     html_content = """
     <!DOCTYPE html>
     <html lang="en">
@@ -101,20 +84,17 @@ def product_details():
     </html>
     """
     
-    # Parse the HTML
     soup = BeautifulSoup(html_content, 'html.parser')
 
-    # Find the product div
     product_div = soup.find('div', class_='product-images')
 
-    product_id = None  # Initialize product_id to None
+    product_id = None 
 
-    # Extract product ID from the href attribute of the anchor tag
     if product_div:
-        product_link = product_div.find('a')  # Find the first anchor tag within the product div
+        product_link = product_div.find('a')
         if product_link:
             href = product_link.get('href')
-            product_id = request.args.get('product_id')  # Extract product_id from the href query parameter
+            product_id = request.args.get('product_id') 
             print("Product ID:", product_id)
         else:
             print("Product link not found")
@@ -137,7 +117,6 @@ def product_details():
         allergens = product_data.get('allergens_tags', [])
         calories = product_data.get('nutriments', {}).get('energy-kcal_100g', 'N/A')
         
-        # Additional nutrition facts
         fat = product_data.get('nutriments', {}).get('fat_100g', 'N/A')
         carbohydrates = product_data.get('nutriments', {}).get('carbohydrates_100g', 'N/A')
         proteins = product_data.get('nutriments', {}).get('proteins_100g', 'N/A')
@@ -157,14 +136,164 @@ def product_details():
     if 'username' in session:
         loggedIn = True
     else: loggedIn = False
-
-    return render_template('product_details.html', name=name, ingredients=ingredients, nutrition=nutrition, image=image, loggedIn=loggedIn, username=session.get('username'), allergens=allergens, calories=calories, fat=fat, carbohydrates=carbohydrates, proteins=proteins, fiber=fiber, sugar=sugar, salt=salt)
+    cart_elements = len(session['cart'])
+    return render_template('product_details.html',product_id=product_id , name=name, ingredients=ingredients, nutrition=nutrition, image=image, loggedIn=loggedIn, username=session.get('username'), allergens=allergens, calories=calories, fat=fat, carbohydrates=carbohydrates, proteins=proteins, fiber=fiber, sugar=sugar, salt=salt, cart_elements=cart_elements)
 
 #                       #
 #  OPEN FOOD FACTS API  #
 #          END          #
 
+@app.route('/addToCart', methods=['POST'])
+def addToCart():
+    if request.form.get('item_id_product_details'):
+        product_id = request.form.get('item_id_product_details')
+        cart = session.get('cart', {})
+        cart[product_id] = cart.get(product_id, 0) + 1
+        session['cart'] = cart
+        print(session['cart'])
+        product = get_product_name(product_id)
+        test = search_products_by_keyword(product)
+        product = test[0]
+        cart_elements = len(session['cart'])
 
+        name = product.get('product_name')
+        nutrition = product.get('nutrition_grades_tags')
+        ingredients = product.get('ingredients_text')
+        image = product.get('image_url')
+    
+        api_url = f"https://world.openfoodfacts.org/api/v0/product/{product_id}.json"
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            product_data = response.json().get('product', {})
+            allergens = product_data.get('allergens_tags', [])
+            calories = product_data.get('nutriments', {}).get('energy-kcal_100g', 'N/A')
+            fat = product_data.get('nutriments', {}).get('fat_100g', 'N/A')
+            carbohydrates = product_data.get('nutriments', {}).get('carbohydrates_100g', 'N/A')
+            proteins = product_data.get('nutriments', {}).get('proteins_100g', 'N/A')
+            fiber = product_data.get('nutriments', {}).get('fiber_100g', 'N/A')
+            sugar = product_data.get('nutriments', {}).get('sugars_100g', 'N/A')
+            salt = product_data.get('nutriments', {}).get('salt_100g', 'N/A')
+        else:
+            allergens = []
+            calories = 'N/A'
+            fat = 'N/A'
+            carbohydrates = 'N/A'
+            proteins = 'N/A'
+            fiber = 'N/A'
+            sugar = 'N/A'
+            salt = 'N/A'
+
+            if 'username' in session:
+                loggedIn = True
+            else: loggedIn = False
+
+            return render_template('product_details.html',product_id=product_id , name=name, ingredients=ingredients, nutrition=nutrition, image=image, loggedIn=loggedIn, username=session.get('username'), allergens=allergens, calories=calories, fat=fat, carbohydrates=carbohydrates, proteins=proteins, fiber=fiber, sugar=sugar, salt=salt, cart_elements=cart_elements)
+    else: 
+        if request.form.get('item_id_product_search'):
+            product_id = request.form.get('item_id_product_search')
+            cart = session.get('cart', {})
+            cart[product_id] = cart.get(product_id, 0) + 1
+            session['cart'] = cart
+            print(session['cart'])
+            product = get_product_name(product_id)
+            search_results = search_products_by_keyword(product)
+            for item in search_results:
+                print("Products:", item.get('product_name'))
+            cart_elements = len(session['cart'])
+            if 'username' in session:
+                loggedIn = True
+                return render_template('search_product.html', search_results=search_results, username=session['username'], loggedIn=loggedIn, cart_elements=cart_elements)
+            else:
+                loggedIn = False
+                return render_template('search_product.html', search_results=search_results, loggedIn=loggedIn, cart_elements=cart_elements)
+    
+def fetch_nutritional_info(product_name):
+    url = f"https://world.openfoodfacts.org/api/v0/product/{product_name}.json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        product_info = response.json()
+        if 'product' in product_info:
+            return product_info['product']
+    return None
+
+def analyze_nutritional_content(cart):
+    total_nutrients = {
+        'calories': 0,
+        'fat': 0,
+        'carbohydrates': 0,
+        'proteins': 0,
+        'fiber': 0,
+        'sugar': 0,
+        'salt': 0
+    }
+
+    for item, details in cart.items():
+        product_info = fetch_nutritional_info(details['name'])
+        if product_info:
+            total_nutrients['calories'] += details['calories']
+            total_nutrients['fat'] += product_info.get('nutriments', {}).get('fat_100g', 0)
+            total_nutrients['carbohydrates'] += product_info.get('nutriments', {}).get('carbohydrates_100g', 0)
+            total_nutrients['proteins'] += product_info.get('nutriments', {}).get('proteins', 0)
+            total_nutrients['fiber'] += product_info.get('nutriments', {}).get('fiber_100g', 0)
+            total_nutrients['sugar'] += product_info.get('nutriments', {}).get('sugars_100g', 0)
+            total_nutrients['salt'] += product_info.get('nutriments', {}).get('salt_100g', 0)
+
+    return total_nutrients
+
+def recommend_similar_products(product_name):
+    recommendations = []
+    url = f"https://world.openfoodfacts.org/cgi/search.pl?search_terms={product_name}&search_simple=1&action=process&json=1"
+    response = requests.get(url)
+    if response.status_code == 200:
+        products = response.json().get('products', [])
+        for product in products[:3]:  # Recommend top 3 similar products
+            recommendations.append(product.get('product_name', 'Unknown'))
+
+    return recommendations
+
+@app.route('/viewCart')
+def viewCart():
+    cart = {}
+    for item in session['cart']:
+        product = get_product_name(item)
+        test = search_products_by_keyword(product)
+        product = test[0]
+        api_url = f"https://world.openfoodfacts.org/api/v0/product/{item}.json"
+        response = requests.get(api_url)
+        product_data = response.json().get('product', {})
+        cart[item] = {
+            'id': item,
+            'name': product.get('product_name'),
+            'photo': product.get('image_url'),
+            'allergens': product_data.get('allergens_tags', []),
+            'calories': product_data.get('nutriments', {}).get('energy-kcal_100g', 'N/A'),
+            'fat': product_data.get('nutriments', {}).get('fat_100g', 'N/A'),
+            'carbohydrates': product_data.get('nutriments', {}).get('carbohydrates_100g', 'N/A'),
+            'proteins': product_data.get('nutriments', {}).get('proteins', 'N/A'),
+            'fiber': product_data.get('nutriments', {}).get('fiber_100g', 'N/A'),
+            'sugar': product_data.get('nutriments', {}).get('sugars_100g', 'N/A'),
+            'salt': product_data.get('nutriments', {}).get('salt_100g', 'N/A')
+        }
+        
+    cart_lenght = len(session['cart'])
+
+    #Recomandations
+    total_nutrients = analyze_nutritional_content(cart)
+    recommendations = {}
+    for nutrient_category in total_nutrients.keys():
+        recommendations[nutrient_category] = recommend_similar_products(nutrient_category)
+    
+    print(recommendations)
+
+    if 'username' in session:
+        loggedIn = True
+        return render_template('cart.html', cart=cart, cart_lenght=cart_lenght, loggedIn=loggedIn, username=session['username'], recommendations=recommendations)
+    else: 
+        loggedIn = False
+        return render_template('cart.html', cart=cart, cart_lenght=cart_lenght, loggedIn=loggedIn, recommendations=recommendations)
+
+
+    
 def create_connection():
     conn = None
     try:
@@ -196,14 +325,14 @@ def create_users_table(conn):
 
 @app.route('/')
 def home():
-    product1_name="Pâte à pizza fine et rectangulaire"
-    product2_name="Amora sce burger 260g" 
-    product3_name="Pesto alla Genovese"
+    if not session['cart']:
+        session['cart'] = {}
+    cart_elements = len(session['cart'])
     if 'username' in session:
         loggedIn = True
-        return render_template('home.html', username=session['username'], loggedIn=loggedIn, product1_name=product1_name, product2_name=product2_name, product3_name=product3_name)
+        return render_template('home.html', username=session['username'], loggedIn=loggedIn, cart_elements=cart_elements)
     else:
-        return render_template('home.html', loggedIn=False)
+        return render_template('home.html', loggedIn=False, cart_elements=cart_elements)
 
 @app.route('/register')
 def register():
@@ -302,16 +431,13 @@ def login_action():
     else:
         return 'Method not allowed'
 
-#Log out function
 @app.route('/logout_action')
 def logOut():
     session.clear()
     return render_template('redirect.html')
 
-#Change profile info
 @app.route('/change_info_action', methods=['POST'])
 def changeInfo():
-    # Extract new information from the form
     new_firstName = request.form['newFirstName']
     new_lastName = request.form['newLastName']
     new_username = request.form['newUsername']
@@ -351,19 +477,16 @@ def redirect():
 # My account route
 @app.route('/myaccount')
 def myaccount():
+    cart_elements = len(session['cart'])
     if 'username' in session:
-        # Get the username from the session
         username = session['username']
 
-        # Connect to the database
         conn = create_connection()
         cursor = conn.cursor()
 
-        # Retrieve user information from the database based on the username
         cursor.execute("SELECT firstName, lastName, username, allergens, dietary_preferences, avatar FROM users WHERE username=?", (username,))
         user_data = cursor.fetchone()  # Fetch the first row
         if user_data:
-            # Unpack the user data
             firstName, lastName, username, allergens, dietary_preferences, avatar = user_data
             print(firstName)
             print(lastName)
@@ -371,8 +494,7 @@ def myaccount():
             print(allergens)
             print(dietary_preferences)
 
-            # Render the template with user data
-            return render_template('myaccount.html', firstName=firstName, lastName=lastName, username=username, allergens=allergens, dietary_preferences=dietary_preferences, avatar=avatar)
+            return render_template('myaccount.html', firstName=firstName, lastName=lastName, username=username, allergens=allergens, dietary_preferences=dietary_preferences, avatar=avatar, cart_elements=cart_elements)
         
 if __name__ == '__main__':
     app.run(debug=True)
